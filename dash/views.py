d100f5cdff41421246c4a667a588b86dda82ed40
nkall <nkallhovde@gmail.com>
2015-07-07 13:54:28 -0700

    Add basic url functionality

--- dash/views.py
+++ dash/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic.list import ListView
+
 
 # Create your views here.
 def index(request):
     return render(request, "dash/index.html")
+
+def userDash(request, userEmail):
+	userEmail = userEmail[:-1]
+	return render(request, 'dash/user/index.html', {'user_email':userEmail})
