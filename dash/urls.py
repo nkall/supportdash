e0617396790e2de96cd3136f243da1a0ba7a6a8e
nkall <nkallhovde@gmail.com>
2015-07-07 16:40:45 -0700

    More basic page display

--- dash/urls.py
+++ dash/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^user/(?P<userEmail>[a-zA-Z0-9_@.]+/$)', views.userDash, name='userDash'),
+    url(r'^user/(?P<userId>[a-zA-Z0-9_@.\-]+/$)', views.userDash, name='userDash'),
 ]
--- dash/views.py
+++ dash/views.py
@@ -2,11 +2,29 @@ from django.shortcuts import render
 from .models import *
 from django.views.generic.list import ListView
 
+import json
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return render(request, "dash/index.html")
+	return render(request, "dash/index.html")
 
-def userDash(request, userEmail):
-	userEmail = userEmail[:-1]
-	return render(request, 'dash/user/index.html', {'user_email':userEmail})
+def userDash(request, userId):
+	userId = userId[:-1]
+	dashGen = userDashGenerator(userId)
+	surveys = dashGen.getSurveyList(0,0)
+	userInfo = dashGen.getUserInfo()	
+	return render(request, 'dash/user/index.html', {'userId':userId, 'surveys':surveys, 'userInfo':userInfo})
+
+class userDashGenerator():
+	def getUserInfo(self):
+		uinfo = Event.objects.raw("SELECT id, user_cache \
+		                           FROM events \
+		                           WHERE user_cache->>'uuid'= %s", [self.uid])[0];
+		return uinfo.user_cache
+
+	def getSurveyList(self, lowerBound, upperBound):
+		return Survey.objects.filter(actor_id=self.uid).order_by('-created_at')
+
+	def __init__(self, userId):
+		self.uid = userId
