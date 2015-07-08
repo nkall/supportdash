90be908364fbf0e3cacf5dc4d211732485d8acf9
nkall <nkallhovde@gmail.com>
2015-07-08 12:17:29 -0700

    All graphs operational!

--- dash/views.py
+++ dash/views.py
@@ -5,7 +5,7 @@ from django.views.generic.list import ListView
 import json
 import pygal
 from pygal.style import *
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Create your views here.
 def index(request):
@@ -27,9 +27,6 @@ def userDash(request, userId):
   posted, and survey responses
 '''
 class InfoObj:
-	def genChart(self):
-		return None
-
 	def __init__(self, userId):
 		self.userId = userId
 		self.count = 0
@@ -37,6 +34,10 @@ class InfoObj:
 		self.chart = None
 
 class TicketsInfo(InfoObj):
+	def genChart(self):
+		tcg = TicketsChart(self.userId, self.objList)
+		return tcg.generate()
+
 	# Returns "true" if the change string indicates a ticket closing
 	def changeIsClose(self, chg):
 		changeType = chg.get('state', False)
@@ -46,14 +47,20 @@ class TicketsInfo(InfoObj):
 
 	def __init__(self, userId):
 		self.userId = userId
-		allChanges = Event.objects.filter(actor_id=self.userId, type='Change')
+		allChanges = Event.objects.filter(actor_id=self.userId, type='Change').order_by('-created_at')
 		self.objList = [chg for chg in allChanges if self.changeIsClose(chg.change)]
+		self.chart = self.genChart()
 		self.count = len(self.objList)
 
 class CommentsInfo(InfoObj):
+	def genChart(self):
+		ccg = CommentsChart(self.userId, self.objList)
+		return ccg.generate()
+
 	def __init__(self, userId):
 		self.userId = userId
-		self.objList = Event.objects.filter(actor_id=self.userId, type='Comment')
+		self.objList = Event.objects.filter(actor_id=self.userId, type='Comment').order_by('-created_at')
+		self.chart = self.genChart()
 		self.count = len(self.objList)
 
 class SurveyInfo(InfoObj):
@@ -64,7 +71,7 @@ class SurveyInfo(InfoObj):
 	def __init__(self, userId):
 		self.userId = userId
 		self.objList = Survey.objects.filter(actor_id=self.userId).order_by('-created_at')
-		self.chart = self.genChart
+		self.chart = self.genChart()
 		self.count = round(100 * (sum([sur.support_score for sur in self.objList]) / len(self.objList)), 1)
 
 
@@ -88,16 +95,25 @@ class Chart:
 		rollingLine = self.genRollingLine(mainLine[1])
 		chart.add(mainLine[0], mainLine[1])
 		chart.add(rollingLine[0], rollingLine[1])
+		chart.x_labels = self.genLabels()
 		return chart.render()
 
 	def genChart(self):
 		return pygal.Line(style = BlueStyle,
+		                  width=350,
+		                  height=350,
                           show_legend=True,
+                          show_x_guides=True,
+                          x_label_rotation=20,
                           legend_at_bottom=True)
 
 	# Must be extended by child classes
 	def genMainLine(self):
 		pass
+
+	# List of all dates from today to some arbitrary date in the past
+	def genDateRange(self):
+		self.dateRange = [datetime.date(datetime.today() - timedelta(days=x)) for x in range(0, 75)]
 
 	def genRollingLine(self, mainLine):
 		rollingLine = []
@@ -114,13 +130,115 @@ class Chart:
 		decayCoefficient = 0.2
 		return decayCoefficient * newVal + (1 - decayCoefficient) * oldAvg
 
+	# Must be extended by child classes
+	def genLabels(self):
+		pass
+
 	def __init__(self, userId):
 		self.uid = userId
+
+
+class TicketsChart(Chart):
+	def genMainLine(self):
+		self.populateDateCount()
+		self.genDateRange()
+		line = []
+		daterange = self.dateRange
+		for date in reversed(daterange):
+			ticketCount = self.ticketDateCount.get(date, 0)
+			label = str(date) + ": " + str(ticketCount) + " comments"
+			line.append({'value': ticketCount,
+			             'label': label})
+		return ('Comments Posted', line)
+
+	# Populates the self.commentDateCount dict with counts for each date
+	def populateDateCount(self):
+		for ticket in self.tickets:
+			ticketDate = datetime.date(ticket.created_at)
+			newCount = self.ticketDateCount.get(ticketDate, 0) + 1
+			self.ticketDateCount[ticketDate] = newCount
+
+	# Same method as in CommentsChart -- this would be better structured w/ inheritance
+	def genLabels(self):
+		dates = self.dateRange
+		labels = []
+		labels.append(dates[0].strftime("%m/%d/%y"))
+		ld = len(dates)
+		if ld == 3:
+			labels.append(dates[int(ld / 2)].strftime("%m/%d/%y"))
+		elif ld > 3:
+			hasLabels = False
+			for i in range(12, 2, -1):
+				if ((ld-1) % i) == 0:
+					for j in range(1, i):
+						labels.append(dates[int(((ld)*j) / i)].strftime("%m/%d/%y"))
+					hasLabels = True
+					break
+			if hasLabels is False:
+				labels.append(dates[int(ld/3)].strftime("%m/%d/%y"))
+				labels.append(dates[int(ld*2 / 3)].strftime("%m/%d/%y"))
+		if ld > 1:
+			labels.append(dates[ld-1].strftime("%m/%d/%y"))
+		return labels
+
+	def __init__(self, userId, tickets):
+		self.uid = userId
+		self.tickets = tickets
+		self.ticketDateCount = {}
+		self.dateRange = []
+
+class CommentsChart(Chart):
+	def genMainLine(self):
+		self.populateDateCount()
+		self.genDateRange()
+		line = []
+		daterange = self.dateRange
+		for date in reversed(daterange):
+			commentCount = self.commentDateCount.get(date, 0)
+			label = str(date) + ": " + str(commentCount) + " comments"
+			line.append({'value': commentCount,
+			             'label': label})
+		return ('Comments Posted', line)
+
+	# Populates the self.commentDateCount dict with counts for each date
+	def populateDateCount(self):
+		for comment in self.comments:
+			commentDate = datetime.date(comment.created_at)
+			newCount = self.commentDateCount.get(commentDate, 0) + 1
+			self.commentDateCount[commentDate] = newCount
+
+	def genLabels(self):
+		dates = self.dateRange
+		labels = []
+		labels.append(dates[0].strftime("%m/%d/%y"))
+		ld = len(dates)
+		if ld == 3:
+			labels.append(dates[int(ld / 2)].strftime("%m/%d/%y"))
+		elif ld > 3:
+			hasLabels = False
+			for i in range(12, 2, -1):
+				if ((ld-1) % i) == 0:
+					for j in range(1, i):
+						labels.append(dates[int(((ld)*j) / i)].strftime("%m/%d/%y"))
+					hasLabels = True
+					break
+			if hasLabels is False:
+				labels.append(dates[int(ld/3)].strftime("%m/%d/%y"))
+				labels.append(dates[int(ld*2 / 3)].strftime("%m/%d/%y"))
+		if ld > 1:
+			labels.append(dates[ld-1].strftime("%m/%d/%y"))
+		return labels
+
+	def __init__(self, userId, comments):
+		self.uid = userId
+		self.comments = comments
+		self.commentDateCount = {}
+		self.dateRange = []
 
 class SurveyChart(Chart):
 	def genMainLine(self):
 		line = []
-		for survey in self.surveys[::-1]:
+		for survey in reversed(self.surveys):
 			label = str(survey.ticket_id) + ': ' + survey.comments
 			line.append({'value': survey.support_score,
 			             'label': label})
@@ -129,11 +247,4 @@ class SurveyChart(Chart):
 	def __init__(self, userId, surveys):
 		self.uid = userId
 		self.surveys = surveys
-
-class CommentsChart(Chart):
-	def genMainLine(self):
-		pass
-
-	def getUserComments(self):
-		pass
 
